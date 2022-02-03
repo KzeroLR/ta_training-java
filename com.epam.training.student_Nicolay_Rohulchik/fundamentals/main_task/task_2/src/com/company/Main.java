41d39de802b279dbdc043675d8239071bd09241a
Kzero <k0lrgluk@gmail.com>
2022-02-03 18:47:59 +0300

    tatakae

---
+++ com.epam.training.student_Nicolay_Rohulchik/fundamentals/main_task/task_2/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for( int i = args.length - 1 ; i >= 0 ; i--)
+            System.out.println("Reverse : " + args[i]);
+
+        }
+    }
