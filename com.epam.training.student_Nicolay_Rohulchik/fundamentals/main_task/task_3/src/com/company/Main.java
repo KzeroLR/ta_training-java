bc1cd7aa4557170aed6d125e66bb3e8b3930006a
Kzero <k0lrgluk@gmail.com>
2022-02-07 17:28:12 +0300

    3

---
+++ com.epam.training.student_Nicolay_Rohulchik/fundamentals/main_task/task_3/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int array [] = {11, 12, 13};
+        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < 3; i++)
+            System.out.println(array[i]);
+
+    }
+}
